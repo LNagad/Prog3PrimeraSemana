390d8c72bdd21b59037154a6a6803b506c12be28
LNagad <nagadgames100k@gmail.com>
2022-05-22 21:05:17 -0400

    prestamos hecho

---
+++ Application/Service/CalcPrestamosService.cs
@@ -0,0 +1,29 @@
+﻿using Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service
+{
+    public class CalcPrestamosService
+    {
+        public double calcularCuotasAPagar(CalcPrestamosViewModel vm)
+        {
+            double cuotaAPagarMensual;
+            try
+            {
+                var interes = vm.montoPrestamo * vm.interes / 100;
+                vm.totalAPagar = vm.montoPrestamo + interes;
+                vm.cuotaAPagarMensual = Math.Round(vm.totalAPagar / vm.cuotasMensuales, 2) ;
+                cuotaAPagarMensual = vm.cuotaAPagarMensual;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return cuotaAPagarMensual;
+        }
+    }
+}
--- FuegoALaLata/Controllers/AppController.cs
+++ FuegoALaLata/Controllers/AppController.cs
@@ -8,9 +8,11 @@ namespace FuegoALaLata.Controllers
     public class AppController : Controller
     {
         private readonly ZodiacoService zodiacoService;
+        private readonly CalcPrestamosService obj;
         public AppController()
         {
             zodiacoService = new();
+            obj = new();
         }
 
         public IActionResult Index()
@@ -28,15 +30,18 @@ namespace FuegoALaLata.Controllers
             if (vm.dDate > 31)
             {
                 ViewBag.zodiacoData = "Hubo un problema con el dia...";
-            } else if (vm.dDate == 0) {
+            }
+            else if (vm.dDate == 0)
+            {
                 ViewBag.zodiacoData = "Hubo un problema con el dia...";
-            } else
+            }
+            else
             {
                 ViewBag.zodiacoData = zodiacoService.getZodiaco(vm);
             }
             return View();
         }
-        public IActionResult Conversion ()
+        public IActionResult Conversion()
         {
             return View();
         }
@@ -44,6 +49,21 @@ namespace FuegoALaLata.Controllers
         {
             return View();
         }
-        
+        [HttpPost]
+        public IActionResult CalPrestamos(CalcPrestamosViewModel vm)
+        {
+             
+            switch (vm.tipoPrestamo)
+            {
+                case 1: vm.interes = 22; break;
+                case 12: vm.interes = 22; break;
+                case 8: vm.interes = 22; break;
+                default: vm.interes = 0; break;
+            }
+
+            var resultado = obj.calcularCuotasAPagar(vm);
+            ViewBag.resultado = resultado;
+            return View("CalPrestamosResultado", vm);
+        }
     }
 }
