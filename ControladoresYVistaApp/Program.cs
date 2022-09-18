bddba55d0d888f39bc1d89d7dd085359c4a4ef15
LNagad <nagadgames100k@gmail.com>
2022-09-18 14:18:49 -0400

    main layout added

--- ControladoresYVistaApp/Program.cs
+++ ControladoresYVistaApp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Zodiaco}/{action=Zodiaco}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
